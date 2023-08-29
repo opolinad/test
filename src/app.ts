701ce74186a00f1b0820cda557956ca253dad61d
Andrés López <andreslop@gmail.com>
2023-08-29 13:54:22 +0200

    add routes and body parse

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,14 @@ import express, { Application } from 'express';
 import * as dotenv from 'dotenv';
 import morgan from 'morgan';
 import { connection } from "./db/models";
+import routes from "./api/routes"
 
 dotenv.config();
 const app: Application = express();
 
 app.use(morgan('dev'));
+app.use(express.json({ limit: '50mb' }));
+app.use('', routes);
 
 connection.authenticate().then((): void => {
     const port = process.env.SERVER_PORT;
