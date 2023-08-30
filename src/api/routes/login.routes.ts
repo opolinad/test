33286695ddb7cc0d8d35fe7a5fd9083af0bc90b4
Andrés López <andreslop@gmail.com>
2023-08-30 09:41:14 +0200

    add middleware to route

--- src/api/routes/login.routes.ts
+++ src/api/routes/login.routes.ts
@@ -1,9 +1,10 @@
 import express, { Router } from 'express';
 import { loginUser } from "../modules/login/login.controller";
+import { loginMiddleware } from "../middlewares/login.middleware";
 
 
 const router: Router = express.Router();
 
-router.post('', loginUser);
+router.post('', loginMiddleware, loginUser);
 
 export default router;
